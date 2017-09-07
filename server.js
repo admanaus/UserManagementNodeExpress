220313269f89aa89c45d216e33d60a5b737f720d
Adrien <admanaus@gmail.com>
2017-09-06 18:01:43 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,113 @@
+#!/usr/bin/env nodemon
+
+const express = require('express');
+const fs = require('fs');
+const dateFormat = require('dateformat');
+const bodyParser = require('body-parser');
+const path = require('path');
+const passport = require('passport');
+const session = require('express-session');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const router = express.Router();
+
+let idCounter = 1;
+let users = [{
+        id: 123,
+        name: 'Sam',
+        email: 'sam@test.com',
+        age: 29
+    },
+    {
+        id: 124,
+        name: 'Kim',
+        email: 'kim@test.com',
+        age: 26
+    }
+];
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(session({secret: 'keyboard cat'}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extend: true }));
+app.use(express.static('views'));
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'This is a title!'
+    });
+});
+
+app.get('/form', (req, res) => {
+
+    res.render('form', {
+        user: {}
+    });
+
+});
+
+app.get('/form/:id', (req, res) => {
+
+    getUser(req.params.id).then((user)=>{
+        res.render('form', {
+            user: user
+        })
+    });
+});
+
+app.get('/users', (req, res) => {
+    res.render('users', {
+        users: users
+    });
+});
+
+app.get('/delete/:id', (req, res) => {
+    console.log('Deleting user id: ' + req.params.id);
+    removeUser(req.params.id);
+    res.redirect('/users');
+});
+
+app.post('/form', (req, res) => {
+
+    let userObject = {
+        id: idCounter,
+        name: req.body.name,
+        email: req.body.email,
+        age: req.body.age
+    };
+
+    users.push(userObject);
+    idCounter++;
+
+    res.redirect('/users');
+});
+
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+});
+
+function removeUser(id) {
+    users.forEach((user, index)=>{
+        if (id == user.id) {
+            users.splice(index, 1);
+        }
+    })
+}
+
+function getUser(id) {
+    return new Promise(executor);
+    function executor(resolve, reject) {
+        users.forEach((user)=>{
+            if (id == user.id) {
+                resolve(user);
+            }
+        });
+        reject({message: 'no user found'})
+    }
+
+}
